f131e1e676bf42af689afc334859a61d6bac24b8
june <wnsgmlb501@naver.com>
2025-04-07 20:50:03 +0900

    hw2

--- hw2/script.js
+++ hw2/script.js
@@ -1,5 +1,4 @@
-
-//todo-submit-btn을 눌렀을 때 input에 입력된 값이 addTodo 함수에 매개변수로 넘겨주게 해주세요! 
+//todo-submit-btn을 눌렀을 때input에 입력된 값이 addTodo 함수에 매개변수로 넘겨주게 해주세요! 
 // input 태그 안에 값을 비우는 것도 잊지마세요! 
 
 //Todo item 컴포넌트입니다. 
@@ -13,7 +12,9 @@ function TodoItem({text,onComplete}){
     `;
 	
 	// 여기에 삭제 버튼을 클릭시 삭제 함수가 호출되게 해주세요 
-	
+	const del = item.querySelector(".delete-btn");
+    del.addEventListener("click",()=>{onComplete(item);})
+
 	return item;
 }
 
@@ -22,5 +23,16 @@ const todoList = document.querySelector('.todo-items');
 //새로운 todo를 추가하는 함수입니다. 
 function addTodo(text) {
     const newItem = TodoItem({ text, onComplete: (item) => item.remove() });
+    todoList.appendChild(newItem);
     //여기에 새로운 todo(newItem)을 todolist에 추가해보세요!
 }
+
+const input = document.querySelector(".todo-input");
+const submitBtn = document.querySelector(".todo-submit-btn");
+submitBtn.addEventListener("click",()=>{
+    const txt = input.value.trim();
+    if(txt!==""){
+        addTodo(txt);
+        input.value="";
+    }
+});
