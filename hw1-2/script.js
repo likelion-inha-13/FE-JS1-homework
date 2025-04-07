9c5810a9aeb0552f54865c44cb4bc5d55fc197db
june <wnsgmlb501@naver.com>
2025-04-07 20:38:13 +0900

    hw1-2

--- hw1-2/script.js
+++ hw1-2/script.js
@@ -1,1 +1,15 @@
-//여기에 코드를 작성해주세요!
+const cl = document.querySelector(".close");
+const op = document.querySelector(".open");
+const menu = document.querySelector(".menu");
+
+function cl_click(){
+  cl.style.display = "none";
+  op.style.display = "inline-block";
+  menu.style.display = "block";
+}
+
+function op_click(){  
+  op.style.display = "none";
+  cl. style.display = "inline-block";
+  menu.style.display = "none";
+}
