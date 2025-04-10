fc813b8d8282b1f94906371b1d4a5f97e3cd99b4
leedasom050115 <kiss9892@inha.edu>
2025-04-10 17:01:03 +0900

    이다솜과제

--- hw1-2/script.js
+++ hw1-2/script.js
@@ -1,1 +1,18 @@
-//여기에 코드를 작성해주세요!
+
+
+const el1 = document.querySelector('.open');
+el1.onclick = function() {
+    const menu = document.querySelect('.menu');
+    menu.style.display = 'none';
+    open.style.display = 'none';
+    document.querySelector('.close').style.display = 'inline';
+};
+
+
+const el1 = document.querySelector('.close');
+el1.onclick = function() {
+    const menu = document.querySelect('.menu');
+    menu.style.display = 'block';
+    open.style.display = 'block';
+    document.querySelector('.open').style.display = 'inline';
+};
