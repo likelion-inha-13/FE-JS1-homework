2981a2dfd1eabf8c4bc8a6bdd25415165bc87f00
ohyesong <yesong9874@naver.com>
2025-04-26 23:24:58 +0900

    멋사js응용과제

--- hw1-2/script.js
+++ hw1-2/script.js
@@ -1,1 +1,17 @@
 //여기에 코드를 작성해주세요!
+
+const sliding = document.querySelector('.sliding');
+const right_t = document.querySelector('.close');
+const bottom_t = document.querySelector('.open');
+const menu = document.querySelector('.menu');
+
+sliding.addEventListener("click", function(event) {
+    if (event.target.classList.contains('close') || event.target.classList.contains('open')) {
+
+        menu.classList.toggle('active'); // .menu .active 추가
+        right_t.classList.toggle('disappear'); // 오른쪽 세모 사라지고 나타나게
+        bottom_t.classList.toggle('appear'); // 아래쪽 세모 나타나고 사라지게
+
+    }  
+})
+
