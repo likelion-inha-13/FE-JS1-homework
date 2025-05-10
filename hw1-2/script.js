3a479a06b4dc961fe601c965428103850c979531
yewonee <hyewon8685@naver.com>
2025-05-10 17:03:50 +0900

    서예원 과제제출

--- hw1-2/script.js
+++ hw1-2/script.js
@@ -1,1 +1,22 @@
 //여기에 코드를 작성해주세요!
+
+const menu = document.querySelector('.menu');
+const click = document.querySelector('.close');
+const Open = document.querySelector('.open');
+
+
+click.addEventListener("click",()=>{
+    menu.classList.add('active');
+    
+    click.style.display="none";
+    Open.style.display="inline-block";
+
+
+});
+
+Open.addEventListener("click",()=>{
+    menu.classList.remove('active');
+    click.style.display="inline-block";
+    Open.style.display="none";
+   
+});
--- hw2/script.js
+++ hw2/script.js
@@ -1,6 +1,17 @@
 
 //todo-submit-btn을 눌렀을 때 input에 입력된 값이 addTodo 함수에 매개변수로 넘겨주게 해주세요! 
 // input 태그 안에 값을 비우는 것도 잊지마세요! 
+const addbtn = document.querySelector('.todo-submit-btn');
+const todoInput = document.querySelector('.todo-input');
+
+addbtn.addEventListener('click', ()=>{
+    const todoText = todoInput.value;  
+    if (todoText !== '') {
+        addTodo(todoText); 
+        todoInput.value = '';
+    }
+
+});
 
 //Todo item 컴포넌트입니다. 
 function TodoItem({text,onComplete}){
@@ -12,7 +23,13 @@ function TodoItem({text,onComplete}){
         <button class="delete-btn">삭제</button>
     `;
 	
-	// 여기에 삭제 버튼을 클릭시 삭제 함수가 호출되게 해주세요 
+	// 여기에 삭제 버튼을 클릭시 삭제 함수가 호출되게 해주세요
+   
+    const delbtn = item.querySelector('.delete-btn'); 
+    delbtn.addEventListener('click', ()=> {
+        onComplete(item); 
+    });
+ 
 	
 	return item;
 }
@@ -23,4 +40,5 @@ const todoList = document.querySelector('.todo-items');
 function addTodo(text) {
     const newItem = TodoItem({ text, onComplete: (item) => item.remove() });
     //여기에 새로운 todo(newItem)을 todolist에 추가해보세요!
+    todoList.appendChild(newItem);
 }
